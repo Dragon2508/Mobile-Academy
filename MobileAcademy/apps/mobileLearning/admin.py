3ff034440ee41660d98977bd88db1266f69fe6fd
Pankratov-Denis <denis-pankratov2000@mail.ru>
2021-10-29 11:43:57 +0300

    Настройка шаблана

--- MobileAcademy/apps/mobileLearning/admin.py
+++ MobileAcademy/apps/mobileLearning/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile, Course, AccessCourse
+
+admin.site.register(Profile)
+admin.site.register(Course)
+admin.site.register(AccessCourse)
