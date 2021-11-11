b1c8b88fe74f462988469254770b02d5b053d065
Pankratov-Denis <denis-pankratov2000@mail.ru>
2021-11-11 16:45:19 +0300

    Интеграция html, переходы

--- MobileAcademy/apps/mobileLearning/views.py
+++ MobileAcademy/apps/mobileLearning/views.py
@@ -1,4 +1,38 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'mobileLearning/testHtml.html')
+    return render(request, 'mobileLearning/main.html')
+
+
+def login(request):
+    return render(request, 'mobileLearning/login.html')
+    
+def registration(request):
+    return render(request, 'mobileLearning/registration.html')
+
+def listOfCourse(request):
+    return render(request, 'mobileLearning/listOfCourse.html')
+
+def account(request):
+    return render(request, 'mobileLearning/account.html')
+
+def lessonOne(request):
+    return render(request, 'mobileLearning/lessonOne.html')
+
+def lessonTwo(request):
+    return render(request, 'mobileLearning/lessonTwo.html')
+
+def lessonThree(request):
+    return render(request, 'mobileLearning/lessonThree.html')
+
+def lessonFour(request):
+    return render(request, 'mobileLearning/lessonFour.html')
+
+def lessonFive(request):
+    return render(request, 'mobileLearning/lessonFive.html')
+
+def lessonSix(request):
+    return render(request, 'mobileLearning/lessonSix.html')
+
+def testing(request):
+    return render(request, 'mobileLearning/testing.html')
