f77d175d3fba649b9418fba39803fbfc7a740c2c
Pankratov-Denis <denis-pankratov2000@mail.ru>
2021-11-11 17:05:20 +0300

    Интеграция html, переходы

--- MobileAcademy/apps/mobileLearning/urls.py
+++ MobileAcademy/apps/mobileLearning/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 
 from . import views
 
+app_name = 'mobileLearning'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='main'),
+    path('login/', views.login, name='login'),
+    path('registration/', views.registration, name='registration'),
+    path('listOfCourse/', views.listOfCourse, name='listOfCourse'),
+    path('account/', views.account, name='account'),
+    path('listOfCourse/lessonOne/', views.lessonOne, name='lessonOne'),
+    path('listOfCourse/lessonTwo/', views.lessonTwo, name='lessonTwo'),
+    path('listOfCourse/lessonThree/', views.lessonThree, name='lessonThree'),
+    path('listOfCourse/lessonFour/', views.lessonFour, name='lessonFour'),
+    path('listOfCourse/lessonFive/', views.lessonFive, name='lessonFive'),
+    path('listOfCourse/Six/', views.lessonSix, name='lessonSix'),
+    path('listOfCourse/testing/', views.testing, name='testing'),
 ]
